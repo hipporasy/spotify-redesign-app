6a9fbeaf5acc7a4b70db340871110c0765b6dd8e
Marasy Phi <hipporasy@gmail.com>
2021-01-03 13:32:38 +0700

    day 2

--- lib/src/screens/home/home.dart
+++ lib/src/screens/home/home.dart
@@ -15,51 +15,110 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: AppColor.primaryBackground,
       body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.dark,
-        child: Container(
-          padding: EdgeInsets.only(
-              left: Dimens.largeMargin, right: Dimens.largeMargin),
-          width: MediaQuery.of(context).size.width,
-          height: 225,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(Images.artistCover), fit: BoxFit.fitWidth),
+        value: SystemUiOverlayStyle.light,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [_buildHeader(), _buildExplore()],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExplore() {
+    return Container(
+      padding: EdgeInsets.only(left: Dimens.normalMargin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Explore',
+            style:
+                TextStyle(fontSize: Dimens.extraLargeFont, color: Colors.white),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
+          _buildExploreCell()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildExploreCell() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          Images.album,
+          width: 120,
+          height: 120,
+        ),
+        Text(
+          'At last',
+          style: TextStyle(color: Colors.white, fontSize: Dimens.normalFont),
+        ),
+        Text('FM - 96')
+      ],
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      padding:
+          EdgeInsets.only(left: Dimens.largeMargin, right: Dimens.largeMargin),
+      width: MediaQuery.of(context).size.width,
+      height: 225,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(Images.artistCover), fit: BoxFit.fitWidth),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Image.asset(
+            Images.artist,
+            width: 100,
+            height: 50,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset(
-                Images.artist,
-                width: 100,
-                height: 50,
+              Text(
+                'New Single',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: Dimens.normalFont, color: AppColor.primary),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('New Single'),
-                  Text('New Single'),
-                ],
+              Text(
+                'Paris In The Rain',
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(fontSize: Dimens.largeFont, color: Colors.white),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    Images.album,
-                    width: 100,
-                    height: 100,
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text('Listen'),
-                  )
-                ],
-              )
             ],
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                Images.album,
+                width: 100,
+                height: 100,
+              ),
+              RaisedButton(
+                color: AppColor.primary,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18)),
+                onPressed: () {},
+                child: Text('Listen',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Dimens.normalFont,
+                    )),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
