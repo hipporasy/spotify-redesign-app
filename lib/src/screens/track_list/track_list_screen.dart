30ab55435fc0a989c547327f3b504d23b6705de7
Marasy Phi <hipporasy@gmail.com>
2021-01-12 23:22:21 +0700

    update track list

--- lib/src/screens/track_list/track_list_screen.dart
+++ lib/src/screens/track_list/track_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/src/theme/colors.dart';
+import 'package:spotify/src/theme/dimens.dart';
 import 'package:spotify/src/theme/images.dart';
+import 'package:spotify/src/theme/textStyle.dart';
 
 class TrackListScreen extends StatefulWidget {
   @override
@@ -7,9 +10,13 @@ class TrackListScreen extends StatefulWidget {
 }
 
 class _TrackListScreenState extends State<TrackListScreen> {
+  bool _isActive = true;
+  bool _isShuffle = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.primaryBackground,
       appBar: AppBar(
         title: Text("At Last"),
         flexibleSpace: Image(
@@ -21,9 +28,113 @@ class _TrackListScreenState extends State<TrackListScreen> {
       ),
       body: Column(
         children: [
+          Container(
+            padding: const EdgeInsets.all(Dimens.normalMargin),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isShuffle = !_isShuffle;
+                    });
+                  },
+                  child: Icon(
+                    Icons.shuffle,
+                    color: _isShuffle ? AppColor.primary : AppColor.lightText,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Offline",
+                      style: AppTextStyle.largeTextStyle,
+                    ),
+                    Switch(
+                      activeColor: AppColor.primary,
+                      value: _isActive,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _isActive = !_isActive;
+                        });
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: Dimens.normalMargin),
+                  child: _buildSideText(),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: 20,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          child: _buildTrackCell(),
+                          margin: const EdgeInsets.only(
+                              left: Dimens.normalMargin,
+                              right: Dimens.normalMargin),
+                        );
+                      }),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSideText() {
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              // this._isTopTrackedSelected = true;
+            });
+          },
+          child: RotatedBox(
+            quarterTurns: 3,
+            child: Text('Tracklist', style: AppTextStyle.primaryLargeTextStyle),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTrackCell() {
+    return Container(
+      margin: const EdgeInsets.only(
+          left: Dimens.normalMargin, right: Dimens.normalMargin),
+      padding: const EdgeInsets.all(Dimens.normalMargin),
+      child: Column(
+        children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Icon(Icons.shuffle), Switch(value: true)],
+            children: [
+              Text(
+                '1 First Yourself',
+                style: AppTextStyle.largeTextStyle,
+              ),
+              Text(
+                '2:56',
+                style: AppTextStyle.lightTextStyle,
+              ),
+            ],
+          ),
+          SizedBox(height: Dimens.normalMargin),
+          Divider(
+            height: 1,
+            color: Colors.grey,
           )
         ],
       ),
